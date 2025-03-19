b4135b9ec1f6b05cbc0282775868f6b62ae5783b
keithunt_35 <144621721+keithunt-35@users.noreply.github.com>
2025-03-19 10:18:33 +0300

    Update header.php

--- indudes/header.php
+++ indudes/header.php
@@ -10,7 +10,7 @@ startSession();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management System</title>
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="icon" href="assets/logo/groupc.jpg"/>
+    
 </head>
 <body>
     <header>
